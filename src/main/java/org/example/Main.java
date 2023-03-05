b700e17233283911ecb1e98b181a363b6c84080b
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2023-03-05 18:16:30 +0300

    Created recursive function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.google.common.io.Files;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,7 +9,10 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     //Replace YOUR_API_KEY with your actual API key
@@ -33,7 +37,8 @@ public class Main {
     public static void main(String[] args) {
         //jSoupParse();
         //jSoupParseFromHtmlText();
-        getFilenamesRecursively();
+        ;
+        getFilenamesRecursively(new File(pathOfFilesToTranslate));
     }
 
     public static void jSoupParse() {
@@ -72,9 +77,41 @@ public class Main {
         //2. loop straight all off them find text and translate text update texts and save as file
     }
 
-    public static void getFilenamesRecursively(){
-        File folder = new File(pathOfFilesToTranslate);
+    public static void getFilenamesRecursively(File folder){
         File[] listOfFiles = folder.listFiles();
+
+        for (File file : listOfFiles){
+            if (file.isDirectory()){
+                getFilenamesRecursively(file);
+                continue;
+            }else {
+                if (file.isFile()){
+                    try {
+                        Document document = Jsoup.parse(file);
+
+
+                        CharSequence charSequence = "asdsadadsa";
+
+
+
+                        List<Element> elements = document.getAllElements().stream()
+                                .filter( element -> element.hasText())
+                                .filter( element -> element.text().contains(charSequence))
+                                .collect(Collectors.toList());
+                        break;
+
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+
+                    //  ##TODO translate
+                    System.out.println(file.getAbsolutePath());
+                }
+            }
+            break;
+
+        }
+    return;
     }
 }
 
